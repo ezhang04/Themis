4b45c8db0ba112799d947cdfb50ca919607b1b4e
priyabriya <priya2816patel@gmail.com>
2025-09-27 16:55:54 -0400

    heehee

---
+++ content.js
@@ -0,0 +1,5 @@
+// This runs automatically on every page
+console.log("Hello! Extension is running on:", window.location.href);
+
+// Example: change background color
+document.body.style.border = "5px solid red";
