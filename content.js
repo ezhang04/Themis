a2c5ee0d6397d9cb7a958bb9518120304478962e
priyabriya <priya2816patel@gmail.com>
2025-09-27 17:16:04 -0400

    another update#

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 // This runs automatically on every page
 console.log("Hello! Extension is running on:", window.location.href);
 
-// Example: change background color
+// Makes it so that script should run when there is a physical
+// change to the screen
 document.body.style.border = "5px solid red";
