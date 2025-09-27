c2407cd90e388ca18b85a377f1cdefac901bd3cf
priyabriya <priya2816patel@gmail.com>
2025-09-27 18:06:02 -0400

    ff

--- content.js
+++ content.js
@@ -3,12 +3,12 @@ console.log("The page URL is:", window.location.href);
 
 // Makes it so that script should run when there is a physical
 // change to the screen
-document.body.style.border = "5px solid red";
+document.body.style.border = "3px solid red";
 
 // Example: display it on the page
 let banner = document.createElement("div");
 banner.textContent = "URL: " + window.location.href;
-banner.style.cssText = "position:fixed;top:0;left:0;background:yellow;padding:5px;z-index:9999;";
+banner.style.cssText = "position:fixed;top:0;left:0;background:blue;padding:5px;z-index:9999;";
 document.body.appendChild(banner);
 
 
